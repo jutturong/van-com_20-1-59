abc93236b6ab94b259716aabee23f7978cbbb86a
jutturong <jutturongcharoenrit@gmail.com>
2016-01-31 12:31:07 +0700

    แก้ err menu

--- vancom/application/views/import_js.php
+++ vancom/application/views/import_js.php
@@ -1,6 +1,16 @@
+     
+<!--
         <link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/themes/default/easyui.css">
         <link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/themes/icon.css">
         <link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/themes/color.css">
         <link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/demo/demo.css">
         <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.min.js"></script>
         <script type="text/javascript" src="http://www.jeasyui.com/easyui/jquery.easyui.min.js"></script>
+    -->
+
+    
+    	<link rel="stylesheet" type="text/css" href="<?=base_url()?>easyui/themes/default/easyui.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>easyui/themes/icon.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>easyui/demo/demo.css">
+	<script type="text/javascript" src="<?=base_url()?>easyui/jquery.min.js"></script>
+	<script type="text/javascript" src="<?=base_url()?>easyui/jquery.easyui.min.js"></script>
